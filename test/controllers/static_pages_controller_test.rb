f52392660ae82843a3b7ad371907eecfc9e27a3a
SamWilkinson <samwilkinson96@mail.com>
2021-12-13 12:34:38 +0100

    Add home page and setup as root route

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Blind Estimation Tool"
+  end
+
+  test "should get home" do
+    get home_path
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+
+
+end
